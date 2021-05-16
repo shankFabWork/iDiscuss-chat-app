79f8999979be80183b399267be1f666cc79c10b4
shankpractisework <83722370+shankpractisework@users.noreply.github.com>
2021-05-16 17:00:30 +0530

    a

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000')
+const socket = io('https://idiscuss-chat-app.herokuapp.com/')
 const form = document.getElementById("send-container")
 const messageInput = document.getElementById("messageInput")
 const messageContainer = document.querySelector(".container")
